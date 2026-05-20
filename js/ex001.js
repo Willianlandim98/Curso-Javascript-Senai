5ddb05a53f6ddd9a2568c77b00167a391b4ac3b5
Willian Landim <landim10@live.com>
2026-05-19 21:40:34 -0300

    Operadores Relacionais JavaScript

---
+++ js/ex001.js
@@ -0,0 +1,27 @@
+// let nome = "Maria"
+// console.log(nome)
+
+// nome = "Jose"
+// console.log(nome)
+
+
+// let nota1 = 4
+// let nota2 = 8
+
+// let media = (nota1 + nota2)/2
+
+// let resultado = media >= 7
+
+// console.log("Sua média foi:", media)
+// console.log(resultado)
+// let nomePessoa = "Marco Aurelio"
+// console.log(typeof nota1)
+// console.log(typeof nomePessoa)
+
+//let teste = "100"
+//console.log (typeof teste)
+// console.log (typeof resultado)
+
+
+let comparacao = 10 === "10"
+console.log(comparacao)
---
+++ js/ex002.js
@@ -0,0 +1,16 @@
+let estouDeFolga = false
+let temSol = true
+
+console.log("Vou ir a praia!",estouDeFolga && temSol)
+
+
+let canetaAzul = true
+let canetaPreta = false
+
+console.log("Vou escrever", canetaAzul || canetaPreta)
+
+let testes = true
+console.log(!testes)
+
+let resultado = (5 > 4 && 20 < 15 || 9 == 9)
+console.log(resultado)

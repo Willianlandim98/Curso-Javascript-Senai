c54ad1b092f8d74be15e678cc3cf4472c05760c5
Willian landim <landim10@live.com>
2026-05-21 21:31:40 -0300

    Array, For, Whale, Orientação em Objeto, Prompt e Alert

---
+++ js/ex007.js
@@ -0,0 +1,43 @@
+// --PROGRAMAÇÃO ORIENTADA A OBJETOS--
+
+
+// let nomeAluno = "João"
+// let idadeAluno = 30
+// let cursoAluno = "Desenvolvimento"
+// let periodoCurso = "Noite"
+
+// console.log(nomeAluno)
+
+const aluno = {
+  nome: "Carlos",
+  idade: 29,
+  curso:{ 
+    nomeCurso:"Desenvolvimento",
+    dataTermino: "09/06/2016",
+    professor: "Jefferson"
+  },
+  periodo: "Noturno",
+}
+
+// console.log(aluno)
+// console.log(aluno.nome)
+// console.log(aluno.periodo)
+// console.log(aluno.curso.nomeCurso)
+// console.log(aluno.curso.dataTermino)
+
+
+// console.table(["Erro no servidor", "testes","testes"])
+
+// ARRAY
+
+const array = [1,2,3,4,5]
+const frutas = ["Maça", "Banana", "Uva"]
+
+console.log(frutas)
+// console.log(frutas[2])
+// console.log(frutas.length)
+// console.log(typeof frutas)
+
+frutas.push('Amora')
+
+console.log(frutas)
